4b932d8a2f06c8975e5d298d607f5c49d7c8ff1e
scgm0 <2682963017@qq.com>
2024-09-14 08:18:39 +0800

    初始化仓库

---
+++ BetterChinese/BetterChineseModSystem.cs
@@ -0,0 +1,22 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Server;
+using Vintagestory.API.Config;
+using Vintagestory.API.Common;
+
+namespace BetterChinese;
+
+public class BetterChineseModSystem : ModSystem {
+
+	// Called on server and client
+	// Useful for registering block/entity classes on both sides
+	public override void Start(ICoreAPI api) { api.Logger.Notification("Hello from template mod: " + api.Side); }
+
+	public override void StartServerSide(ICoreServerAPI api) {
+		api.Logger.Notification("Hello from template mod server side: " + Lang.Get("betterchinese:hello"));
+	}
+
+	public override void StartClientSide(ICoreClientAPI api) {
+		api.Logger.Notification("Hello from template mod client side: " + Lang.Get("betterchinese:hello"));
+	}
+
+}
